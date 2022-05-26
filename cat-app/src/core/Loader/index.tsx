29cde6e87fc8e5feb73a485f9f43db5a4467a76e
Gagik-arch <75838391+Gagik-arch@users.noreply.github.com>
2022-05-26 16:50:28 +0400

    Delete cat-app directory

--- cat-app/src/core/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-import s from './index.module.css'
-
-export const Loader: FC = () => {
-
-    return (
-        <div className={s['lds-roller']}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    )
-}
